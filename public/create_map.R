bf9689fbd779656345579685cd8e2d1e3b321f7d
Brooke Wager <bwager@olin.edu>
2024-02-18 06:48:24 -0500

    Fixed hyperlinks in map

--- public/create_map.R
+++ public/create_map.R
@@ -3,7 +3,7 @@ library(leaflet.extras)
 library(sf)
 
 # Read the shapefile
-districts <- read_sf(dsn = "districts", layer = "districts") %>% filter(TOWN %in% c('BOSTON'))
+districts <- read_sf(dsn = "districts_shp", layer = "districts_shp") %>% filter(TOWN %in% c("BOSTON"))
 # Color each ward
 colorScale <- colorNumeric(palette = rainbow(7), domain = as.integer(districts$WARD) %% 11)
 
@@ -12,7 +12,7 @@ leaflet(districts) %>%
   addTiles() %>%
   addPolygons(
     label = ~paste0(TOWN, ": Ward ", WARD, ", Precinct ", PRECINCT),
-    popup = ~paste0('<a href=localhost:3000/learn_more/', WARD, '/', PRECINCT, '>', TOWN, ": Ward ", WARD, ", Precinct ", PRECINCT),
+    popup = ~paste0('<a href=http://localhost:3000/learn_more/', WARD, '/', PRECINCT, '>', TOWN, ": Ward ", WARD, ", Precinct ", PRECINCT),
     fillOpacity = 0.5,
     fillColor = ~colorScale(as.integer(districts$WARD) %% 11),
     color="black",
