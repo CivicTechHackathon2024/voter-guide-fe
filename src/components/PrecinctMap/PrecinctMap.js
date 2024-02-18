5d07eb95373233c935abea1ae02f300cedb2e5f9
Brooke Wager <bwager@olin.edu>
2024-02-18 08:28:28 -0500

    Removed obsolete link in map page

--- src/components/PrecinctMap/PrecinctMap.js
+++ src/components/PrecinctMap/PrecinctMap.js
@@ -5,10 +5,6 @@ function PrecinctMap() {
   return (
     <div className="iframe-container">
       <iframe src="/create_map.html" title="learn more" ></iframe>
-      <Link to='/learn_more'>
-      <p>find a candidate</p>
-      </Link>
-      
     </div>
   )
 }
