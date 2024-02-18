ba75a1d2643791d47a88ed2102d59c2bb434e8cf
KingKimSilverWolf <129137737+KingKimSilverWolf@users.noreply.github.com>
2024-02-18 09:15:37 -0500

    Update on navbar and How To Vote Page

--- src/components/HowToVote/HowToVote.js
+++ src/components/HowToVote/HowToVote.js
@@ -32,7 +32,7 @@ function HowToVote() {
                 <span className="rule-title">Identification Requirements:</span> While Massachusetts does not require voters to show identification at the polls, <b style={{ color: '#c00' }}>first-time voters,</b> who registered by mail may be asked to provide ID or a copy of a <b style={{ color: '#c00' }}>current utility bill, bank statement, government check, paycheck, or other government document showing their name and address</b>.
               </li>
             </ul>
-            <button> <b>Learn more ...</b>
+            <button> <b>Learn more...</b>
             </button>
           </div>
 
