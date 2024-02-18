b2369b5d7b98a9869291b860144c7b05c07b5120
Jen Ziel <jenziel400@gmail.com>
2024-02-18 08:31:12 -0500

    Add how to vote content. update nav link for candidate info. add styling

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -1,14 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import '../LandingPage/LandingPage.css';
+import arrow from "../../images/arrow.png";
 function LandingPage() {
   return (
+    <main className='landing-page-main'>
+
     <div className='landing-page-wrapper'>
-      <h1>VOTER</h1>
-      <h1>GUIDE</h1>
+      <h1 className='landing-page-title top'>VOTER</h1>
+      <h1 className='landing-page-title bottom'>GUIDE</h1>
       <Link to='/map'>
-        <p>Find Out More</p>
+        <p className='landing-page-btn'>Find Out More</p>
       </Link>
+      <div className='landing-page-circle'></div>
+      <img src={arrow} className='landing-page-arrow rotated-arrow'></img>
     </div>
+    </main>
   );
 }
 
