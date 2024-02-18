b8118abe5a4a892cec9566f1524b03f1fd38e598
Brooke Wager <bwager@olin.edu>
2024-02-18 09:17:46 -0500

    added instructions on map

--- src/components/PrecinctMap/PrecinctMap.js
+++ src/components/PrecinctMap/PrecinctMap.js
@@ -3,8 +3,11 @@ import '../PrecinctMap/PrecinctMap.css'
 import {Link } from 'react-router-dom'
 function PrecinctMap() {
   return (
-    <div className="iframe-container">
-      <iframe src="/create_map.html" title="learn more" ></iframe>
+    <div>
+      <h2>Scroll over map to view the ward and precinct. Click on a district to view its ballot. Use the magnifying glass button to search for an address.</h2>
+      <div className="iframe-container">
+          <iframe src="/create_map.html" title="learn more" ></iframe>
+      </div>
     </div>
   )
 }
