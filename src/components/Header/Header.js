3981afdc3619ea9afda9f7648bdde6d91bc431fb
Jen Ziel <jenziel400@gmail.com>
2024-02-18 06:39:11 -0500

    Combine the Card styling to the main branch.  Separate app into more components.  Create a Footer component.  Create a Header component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "../Header/Header.css";
+import { Link, NavLink } from "react-router-dom";
+
+function Header() {
+  return (
+    <header>
+      <Link to="/" style={{ textDecoration: 'none' }}>
+        <h1 className='logo'>Boston City</h1>
+      </Link>
+      <nav>
+        <NavLink to='/' className="nav-title">Home</NavLink>
+        <NavLink to='/learn_more' className="nav-title">Candidate Info</NavLink>
+        <NavLink to='/how_to_vote' className="nav-title">How to Vote</NavLink>
+        <NavLink className="nav-title">Contact</NavLink>
+      </nav>
+
+      <button class='button type1'></button>
+    </header>
+  );
+}
+
+export default Header;
