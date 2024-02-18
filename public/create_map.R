8004be3c27fe0188b04307e37153975a409735a8
Brooke Wager <bwager@olin.edu>
2024-02-18 07:05:54 -0500

    Fixed opening links in iframe

--- public/create_map.R
+++ public/create_map.R
@@ -1,9 +1,10 @@
 library(leaflet)
 library(leaflet.extras)
+library(dplyr)
 library(sf)
 
 # Read the shapefile
-districts <- read_sf(dsn = "districts_shp", layer = "districts_shp") %>% filter(TOWN %in% c("BOSTON"))
+districts <- read_sf(dsn = "districts_shp", layer = "districts") %>% filter(TOWN %in% c("BOSTON"))
 # Color each ward
 colorScale <- colorNumeric(palette = rainbow(7), domain = as.integer(districts$WARD) %% 11)
 
@@ -12,7 +13,7 @@ leaflet(districts) %>%
   addTiles() %>%
   addPolygons(
     label = ~paste0(TOWN, ": Ward ", WARD, ", Precinct ", PRECINCT),
-    popup = ~paste0('<a href=http://localhost:3000/learn_more/', WARD, '/', PRECINCT, '>', TOWN, ": Ward ", WARD, ", Precinct ", PRECINCT),
+    popup = ~paste0('<a href=http://localhost:3000/learn_more/', WARD, '/', PRECINCT, ' target="_blank">', TOWN, ": Ward ", WARD, ", Precinct ", PRECINCT),
     fillOpacity = 0.5,
     fillColor = ~colorScale(as.integer(districts$WARD) %% 11),
     color="black",
