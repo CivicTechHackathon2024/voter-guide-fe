9d7c5480c8535b611027613350729389e11ed71f
Jen Ziel <jenziel400@gmail.com>
2024-02-18 10:54:53 -0500

    format the title on the map page.  Remove the non-functional text input field

--- src/components/PrecinctMap/PrecinctMap.js
+++ src/components/PrecinctMap/PrecinctMap.js
@@ -5,8 +5,7 @@ function PrecinctMap() {
   return (
     <div>
       <p className='map-title'>Let's Find Your Precinct</p>
-      <input type='text' placeholder='type in your street address' className='map-text-input'></input>
-    
+    <p className='map-subtitle'>Type in your address to get your unique ballot</p>
       <div className="iframe-container">
           <iframe src="/create_map.html" title="learn more" ></iframe>
       </div>
